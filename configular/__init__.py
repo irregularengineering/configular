274cacd13dfef334d671cb65b88d2ec8b4959e11
reed-only <mattreed79@gmail.com>
2020-07-02 21:16:51 -0700

    CLI (#10)

--- configular/__init__.py
+++ configular/__init__.py
@@ -7,7 +7,7 @@ _________                _____.__             .__
         \/            \/        /_____/                 \/
 """
 
-__version__ = '0.2.2'
+__version__ = '0.3.0'
 
 from configular.configular import Configular
 from configular.configular import ParameterNotFoundException
